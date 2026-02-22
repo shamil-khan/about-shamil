b958d53e6ed44580b70ba5a7c6ee426f85741a63
shamil-khan <shamil.ahmed@gmail.com>
2026-02-22 07:10:41 +0500

    [pkg] pnpm add tailwindcss @tailwindcss/vite -D

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint2';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,9 +11,7 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler', { target: '19' }]],
       },
     }),
-    eslint({
-      cache: true,
-      fix: true, // Optional: automatically fix fixable errors on save
-    }),
+    tailwindcss(),
+    eslint(),
   ],
 });
