5c14edceb4ad4662bdee776bdc876e39d660f483
shamil-khan <shamil.ahmed@gmail.com>
2026-03-06 18:48:43 +0500

    temporary added load via api

--- apps/web/src/hooks/useCVData.ts
+++ apps/web/src/hooks/useCVData.ts
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
 import type { CVData } from '@/types/cv';
-// Static import for now - will be replaced with API call
-// import cvDataJson from '@/data/cv-data.json';
 import { documentApiClientSafe } from '@/api-wrappers/DocumentApi/DocumentApiClient';
 import { toast } from 'sonner';
+// Static import for now - will be replaced with API call
+import cvDataJson from '@/data/cv-data.json';
 
 interface UseCVDataOptions {
   locale?: string;
@@ -29,27 +29,31 @@ export function useCVData(options: UseCVDataOptions = {}): UseCVDataReturn {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        const useApi = false;
         setIsLoading(true);
         setError(null);
-        // setData(cvDataJson as CVData);
 
-        const response = await documentApiClientSafe.getIdentity('test-user');
-        console.log('reponse', response);
-        if (!response.success) {
-          toast.error(`Failed to load document: ${response.error}`);
-          return;
-        }
-        const document = response.data;
-        if (document.content.type === 'inline') {
-          console.log('reponse - content', document.content);
-          setData(document.content.data as CVData);
-        } else if (document.content.type === 'chunked') {
-          toast.error(`Failed to load document: ${document.content}`);
-        } else if (document.content.type === 'external') {
-          toast.error(`Failed to load document: ${document.content}`);
-        }
+        if (useApi) {
+          const response = await documentApiClientSafe.getIdentity('test-user');
+          console.log('reponse', response);
+          if (!response.success) {
+            toast.error(`Failed to load document: ${response.error}`);
+            return;
+          }
+          const document = response.data;
+          if (document.content.type === 'inline') {
+            console.log('reponse - content', document.content);
+            setData(document.content.data as CVData);
+          } else if (document.content.type === 'chunked') {
+            toast.error(`Failed to load document: ${document.content}`);
+          } else if (document.content.type === 'external') {
+            toast.error(`Failed to load document: ${document.content}`);
+          }
 
-        console.log('reponse', response);
+          console.log('reponse', response);
+        } else {
+          setData(cvDataJson as CVData);
+        }
       } catch (err) {
         setError(
           err instanceof Error ? err : new Error('Failed to load CV data'),
